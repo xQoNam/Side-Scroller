f5daaab64823503e9bfb237d7a1f8b32e4c81e49
xQoNam <kandeckigs1234@interia.pl>
2019-03-15 19:38:47 +0100

    cleaned a bit camera controller

--- 2D Side Crawler/Assets/Scripts/CameraController.cs
+++ 2D Side Crawler/Assets/Scripts/CameraController.cs
@@ -9,12 +9,26 @@ public class CameraController : MonoBehaviour
     public float lerpSpeed;
     public Vector3 offset;
     private Vector3 velocity = Vector3.zero;
+    float leftLimitX, rightLimitX, downLimitY, upLimitY;
+
+    private void Awake()
+    {
+        setCameraLimits();
+    }
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
-        desiredPosition = new Vector3(Mathf.Clamp(desiredPosition.x, limiters[0].transform.position.x +15, limiters[1].transform.position.x - 15), Mathf.Clamp(desiredPosition.y, limiters[2].transform.position.y + 8.5f, limiters[3].transform.position.y - 8.5f), -10);
+        desiredPosition = new Vector3(Mathf.Clamp(desiredPosition.x, leftLimitX, rightLimitX), Mathf.Clamp(desiredPosition.y, downLimitY, upLimitY), -10);
         Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, lerpSpeed);
         transform.position = smoothedPosition;
+    }
+
+    void setCameraLimits()
+    {
+        leftLimitX = limiters[0].transform.position.x + 15;
+        rightLimitX = limiters[1].transform.position.x - 15;
+        downLimitY = limiters[2].transform.position.y + 8.5f;
+        upLimitY = limiters[3].transform.position.y - 8.5f;
     }
 }
